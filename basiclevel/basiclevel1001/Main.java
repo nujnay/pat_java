b3f4f4c0a6491853a15edcf1e01a1a6e4497262d
820459436@qq.com <20170611>
2018-04-30 20:55:14 +0800

    master

--- basiclevel/basiclevel1001/Main.java
+++ basiclevel/basiclevel1001/Main.java
@@ -1,17 +1,24 @@
 package basiclevel1001;
 
-
 import java.util.Scanner;
-
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("rrr");
 		Scanner in = new Scanner(System.in);
+		int count = 0;
 		int number = in.nextInt();
+		while (number != 1) {
+			count++;
+			if (number % 2 == 0) {
+				number = number / 2;
+			} else {
+				number = (number * 3 + 1) / 2;
+			}
+
+		}
 		in.close();
-		System.out.println("rrr");
+		System.out.println(count+"");
 	}
 
 }
