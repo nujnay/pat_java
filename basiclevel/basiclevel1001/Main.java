ed285813cc64826c83e1933d4228a145cfc870d8
820459436@qq.com <20170611>
2018-04-30 20:49:48 +0800

    master

---
+++ basiclevel/basiclevel1001/Main.java
@@ -0,0 +1,17 @@
+package basiclevel1001;
+
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("rrr");
+		Scanner in = new Scanner(System.in);
+		int number = in.nextInt();
+		in.close();
+		System.out.println("rrr");
+	}
+
+}
