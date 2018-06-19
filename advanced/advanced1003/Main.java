fb29f0d169d86c4631601e53371c74ec205dc7ec
820459436@qq.com <20170611>
2018-06-19 21:08:09 +0800

    master

---
+++ advanced/advanced1003/Main.java
@@ -0,0 +1,9 @@
+package advanced1003;
+
+public class Main {
+/*
+ * 紧急情况 作为城市的救援队的队长
+ * 你被给 与一张这个城市特殊的地图
+ */
+}
+
--- basiclevel/basiclevel1053/Main.java
+++ basiclevel/basiclevel1053/Main.java
@@ -1,5 +1,10 @@
 package basiclevel1053;
 
 public class Main {
+	//可能空置 柏树
+	//房子数量
+	public static void main(String[] args){
+		
+	}
 
 }
